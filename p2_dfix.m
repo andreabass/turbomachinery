977f36e20cbdb1edee7413fc3d0dcf54bd59bea3
Paolo <paolobarbato95@gmail.com>
2019-07-04 20:17:55 +0200

    Aggiornamento

--- p2_dfix.m
+++ p2_dfix.m
@@ -7,38 +7,13 @@
                %%%%%
                 %%%
                  %
-        
-                 
-        %%%%%% [INITIALIZATION] %%%%%%%
-        %Initialization value for V_2A_m
-        V_2A_m = V_1A_m;
-        V_2A_t = V_2A_m;
-        V_2A_h = V_2A_m;
-        
-        % Let's define initialization values for inlet axial velocity and
-        % rotor efficiency defined as the ratio between isentropic static
-        % enthalpy change and real static enthalpy change. Thanks to these
-        % values we'll be able to fully determine the VT and TDN quantities
-        % at the rotor outlet that will allow us to calculate losses and
-        % therefore get to adjusted values of the TDN quantities. Such
-        % updated values will be finally used to recalculate the efficiency
-        % of the rotor.In the meanwhile for every cycle we'll impose the
-        % mass balance to update the axial velocity.
-        
-        V_2A = [V_2A_m V_2A_m + 2*tol];
-        eta_R_t = [eta_TT_m - 2*tol eta_TT_m];
-        eta_R_m = [eta_TT_m - 2*tol eta_TT_m];
-        eta_R_h = [eta_TT_m - 2*tol eta_TT_m];
-        
-        l_Eu = deltaHis_TT / eta_TT_m;
-%        while abs(eta_R_t(end) - eta_R_t(end-1))> tol || abs(eta_R_m(end) - eta_R_m(end-1))> tol || abs(eta_R_h(end) - eta_R_h(end-1))> tol
-        
+            
+        %%% INITIALIZATION %%%
+
+        % To initialize the velocity triangle at the outlet of the rotor
+        % we refer to an equivalent problem in which the losses are 
+        % assumed through the condition eta_R,j = eta_TT for j = m,t,h
+
         p2_init
-        
-        %   HOWELL CORRELATION %
-        Re_How = 3e5;
-        sigma_R_m = 1;
-        
-        %Chord calculation based on Howell value for Reynolds number
          
 
