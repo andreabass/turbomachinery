afff0cd367349b266df1fb415ec13601344f897b
Paolo <paolobarbato95@gmail.com>
2019-07-06 22:56:14 +0200

    correction

--- results.m
+++ results.m
@@ -4,6 +4,6 @@ disp(['Static-to-static row efficiencies      :     ', num2str([eta_IGV_av eta_R
 disp(['Pressure loss coefficients             :     ', num2str([Y_p_IGV_av Y_p_R_av Y_p_S_av])])
 disp(['Rotor diffusion coefficients (h/m/t)   :     ', num2str([Dh Dm Dt])])
 disp(['Stator diffusion coefficients (h/m/t)  :     ', num2str([DhS DmS DtS])])
-disp(['Reaction degree at midspam          :     ', num2str(Xi_m)])
+disp(['Reaction degree at midspan             :     ', num2str(Xi_m)])
 %disp(['According to Howell Statistics      :     ', Howell_R, Howell_S])
 
