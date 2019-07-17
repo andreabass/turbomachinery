200998ab0516c7c0aaff12988f2730d6541b3aec
CarloGaeta <47579050+CarloGaeta@users.noreply.github.com>
2019-07-17 21:36:20 +0200

    New chord

--- V2_macheffect/options.m
+++ V2_macheffect/options.m
@@ -1,5 +1,5 @@
 format shortG
 
-HOW_OPT = 0;        % If 1: Howell optimization procedure for the solidity
+HOW_OPT = 1;        % If 1: Howell optimization procedure for the solidity
 DR = 0;
 tol = 1e-6;         % Relative tolerance
